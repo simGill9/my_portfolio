0122795c3a565ceff5b34daca5bb464b3de390af
Simar Gill <simarpreetkaur798@gmail.com>
2023-11-21 20:05:03 -0800

    Add wireframe #issue2

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ document
     alert("Message sent!");
     // Here you can add more functionality, like sending the data to a server
   });
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  const hour = new Date().getHours();
+  const welcomeText = document.getElementById("welcome-text");
+  if (hour < 12) {
+    welcomeText.innerText = "Good Morning! Welcome to my page.";
+  } else if (hour < 18) {
+    welcomeText.innerText = "Good Afternoon! Welcome to my page.";
+  } else {
+    welcomeText.innerText = "Good Evening! Welcome to my page.";
+  }
+});
